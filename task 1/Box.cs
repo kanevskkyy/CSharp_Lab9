185b96a1eb1d9a66370326c4bf7364cd7dacd48d
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-20 18:00:15 +0300

    done with task 1

---
+++ task 1/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"System32.{typeof(T).Name} : {Value}";
+        }
+    }
+}
--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using task_1;
+
+class Task
+{
+    static void Main()
+    {
+        Box<int> first = new Box<int>(123123);
+        Console.WriteLine(first);
+
+        Box<string> second = new Box<string>("life is good");
+        Console.WriteLine(second);
+
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
